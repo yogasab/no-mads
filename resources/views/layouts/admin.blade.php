a964315d6a52560727b6fbac0d4572a482c8a0ac
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-31 14:18:49 +0700

    Separate styles, css, and js admin components

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,14 +11,7 @@
 
   <title>Admin</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="{{ url('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="{{ url('backend/css/sb-admin-2.min.css') }}" rel="stylesheet">
+  @include('components.admin.styles')
 
 </head>
 
@@ -42,15 +35,7 @@
       </div>
       <!-- End of Main Content -->
 
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; NoMads 2022</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
+      @include('components.admin.footer')
 
     </div>
     <!-- End of Content Wrapper -->
@@ -83,22 +68,7 @@
     </div>
   </div>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="{{ url('backend/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ url('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="{{ url('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="{{ url('backend/js/sb-admin-2.min.js') }}"></script>
-
-  <!-- Page level plugins -->
-  <script src="{{ url('backend/vendor/chart.js/Chart.min.js') }}"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="{{ url('backend/js/demo/chart-area-demo.js') }}"></script>
-  <script src="{{ url('backend/js/demo/chart-pie-demo.js') }}"></script>
+  @include('components.admin.scripts')
 
 </body>
 
