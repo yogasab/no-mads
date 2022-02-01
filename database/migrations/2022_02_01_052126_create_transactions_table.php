fd5e5c3c33707284334604b5f746e73fa3a1a860
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-02-01 13:11:20 +0700

    Create model, migration for Transaction and Transaction Detail

---
+++ database/migrations/2022_02_01_052126_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('travel_packages_id');
+            $table->foreignId('users_id')->nullable();
+            $table->integer('additional_visa');
+            $table->integer('transaction_total');
+            $table->enum('transaction_status', ['IN_CART', 'PENDING', "SUCCESS", 'FAILED']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
