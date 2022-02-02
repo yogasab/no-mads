8e20107da02a11b008486615fd0f1a5e60e4ecbb
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-02-02 12:10:35 +0700

    Create StoreTravelPackage validation request and fill the fillable TravelPackage model

---
+++ app/Http/Requests/Admin/Travel/StoreTravelPackageRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin\Travel;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreTravelPackageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'location' => 'required|max:255',
+            'description' => 'required',
+            'featured_events' => 'required|max:255',
+            'languages' => 'required|max:255',
+            'foods' => 'required|max:255',
+            'departure_date' => 'required|date',
+            'duration' => 'required|max:255',
+            'type' => 'required|max:255',
+            'price' => 'required|integer'
+        ];
+    }
+}
--- app/Models/TravelPackage.php
+++ app/Models/TravelPackage.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TravelPackage extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'location',
+        'description',
+        'featured_events',
+        'languages',
+        'foods',
+        'departure_date',
+        'duration',
+        'type',
+        'price'
+    ];
+
+    protected $hidden = [];
 }
