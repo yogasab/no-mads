ca332511fc58e074354cfd02373e6fae6fa4c699
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-02-04 12:55:44 +0700

    Create fillable property and relationship in Transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function transaction_details(): HasMany
+    {
+        return $this->hasMany(TransactionDetail::class, 'transactions_id', 'id');
+    }
+
+    public function transacation_package(): BelongsTo
+    {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
